7ea83e0b5df345468f40f262486a7c1c5ec7690e
Dudok_kh03@student.itstep.org <Dudok_kh03@student.itstep.org>
2024-12-16 16:49:14 +0200

    Add project files.

---
+++ 01_Intro_to_C-sharp/Program.cs
@@ -0,0 +1,124 @@
+﻿internal class Program
+{
+    static void InputSymb()
+    {
+        Console.WriteLine("Enter one symbol : ");
+        char symbol = (char)Console.Read();
+        Console.WriteLine("Symbol :: "+ symbol);
+        Console.WriteLine("IsLetter :: " + char.IsLetter(symbol));
+        Console.WriteLine("IsUpper :: " + char.IsUpper(symbol));
+        Console.WriteLine("IsDigit :: " + char.IsDigit(symbol));
+    }
+
+    private static void Main(string[] args)
+    {
+        //InputSymb();
+        //Task();
+        //Console.WriteLine("Enter Line :: ");
+        //string text = Console.ReadLine();
+
+        //double res = Convert.ToDouble(text);
+
+        //var res = int.Parse(text);
+
+        //int res;
+        //if (int.TryParse(text, out res))
+        //{
+        //    res = res + 10;
+        //    Console.WriteLine($"Result = {res}");
+        //}
+
+        //Console.WriteLine("Enter number of day :: ");
+        //int day = int.Parse(Console.ReadLine());
+
+        ////if (day == 1)
+        ////{
+        ////    Console.WriteLine("Monday");
+        ////}
+        ////else if (day == 2)
+        ////{
+        ////    Console.WriteLine("Tuersday");
+        ////}
+        ////else
+        ////{
+        ////    Console.WriteLine("Error");
+        ////}
+        //switch (day)
+        //{
+        //    case 1:
+        //        Console.WriteLine("Monday");
+        //        break;
+        //    case 2:
+        //        Console.WriteLine("Tuersday");
+        //        break;
+        //    default:
+        //        Console.WriteLine("Error");
+        //        break;
+        //}
+
+        //int i = 0;
+        //for (; ; )
+        //{
+        //    i++;
+        //    Console.WriteLine(i + "Hello");
+        //    if (i >= 5) break;
+        //}
+        //ConsoleKey key;
+        //while ((key = Console.ReadKey().Key) != ConsoleKey.Escape)
+        //{
+        //    switch (key)
+        //    { 
+        //        case ConsoleKey.UpArrow:
+        //            Console.WriteLine("UpArrow");
+        //            break;
+        //        case ConsoleKey.DownArrow:
+        //            Console.WriteLine("DownArrow");
+        //            break;
+        //        case ConsoleKey.LeftArrow:
+        //            Console.WriteLine("LeftArrow");
+        //            break;
+        //        case ConsoleKey.RightArrow:
+        //            Console.WriteLine("RightArrow");
+        //            break;
+        //        default:
+        //            break;
+
+        //    }
+        //}
+        int res;
+        do
+        {
+            Console.WriteLine("2 + 2 = ");
+            res = int.Parse(Console.ReadLine());
+        } while (res != 4);
+
+
+
+    }
+    static void Task()
+    {
+        //Console.BackgroundColor = ConsoleColor.Green;
+        //Console.ForegroundColor = ConsoleColor.Red;
+        //Console.CursorLeft = 40;
+        //Console.CursorTop = 40;
+        Console.OutputEncoding = System.Text.Encoding.Unicode; // вивід кирилиці
+        Console.InputEncoding = System.Text.Encoding.Unicode; // ввід кирилиці
+        string line = "C# language - популярна мова під .NET";
+        string str = "Hello!!!";
+        Console.WriteLine(line);
+        double d = 23.224353;
+        decimal money = 123_456_890_123m;
+        float f = 189.34f;
+        var v = 777.888;
+
+        // 1) write
+        Console.WriteLine("Money = " + money);
+
+        // 2) format line
+        Console.WriteLine("Float value f = {0} /t SQRT = {1}", f, Math.Sqrt(f));
+
+        // 3) imterplation line
+        Console.WriteLine($"Message :: {str}");
+        Console.WriteLine($"Value v * 2 = {v * 2,-20}  ++ f = {++f}");
+    }
+}
---
+++ 01_Intro_to_C_sharp_ HW/Program.cs
@@ -0,0 +1,106 @@
+﻿using static System.Console;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        WriteLine("\t\tN1");
+        WriteLine("\tAll positions: intern, junior, specialist, manager, director");
+        Write("\tEnter position: ");
+        string position = ReadLine();
+        Write("\tEnter number of hours: \t");
+        int hours = int.Parse(ReadLine());
+        int result = 0;
+        switch (position)
+        {
+            case "intern":
+                result = hours * 100;
+                break;
+            case "junior":
+                result = hours * 150;
+                break;
+            case "specialist":
+                result = hours * 200;
+                break;
+            case "manager":
+                result = hours * 300;
+                break;
+            case "director":
+                result = hours * 500;
+                break;
+            default:
+                break;
+        }
+        WriteLine($"\tResult = {result} UAH");
+        WriteLine("\t\tN2");
+        Write("\tEnter start: \t"); int start = int.Parse(ReadLine());
+        Write("\tEnter end: \t"); int end = int.Parse(ReadLine());
+        for (int i = start; i <= end; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Write(i);
+            }
+            WriteLine();
+        }
+
+        WriteLine("\t\tN3");
+        int count_space = 0;
+        int count_lett = 0;
+        int count_numb = 0;
+        int count_symb = 0;
+
+        while (true)
+        {
+            Write("\tEnter one symbol: ");
+            string input = ReadLine();
+            if (string.IsNullOrEmpty(input))
+            {
+                continue;
+            }
+            char one_char = input[0];
+            if (char.IsDigit(one_char))
+                count_numb++;
+            else if (char.IsLetter(one_char))
+                count_lett++;
+            else if (char.IsWhiteSpace(one_char))
+                count_space++;
+            else if (one_char == '.')
+            {
+                count_symb++;
+                break;
+            }
+            else
+                count_symb++;
+            one_char = '\0';
+        }
+        WriteLine($"\tResult: \n\tspace = {count_space}\n\tletter = {count_lett}\n\tdigit = {count_numb}\n\tsymbol = {count_symb}");
+        
+        WriteLine("\t\tN4");
+        // щоб продовжити кожного разу потрібно натиснути Enter
+        ConsoleKey key;
+        char one_letter;
+        WriteLine("\t\tEsc - Exit\t\tEnter - continue");
+        while ((key = Console.ReadKey().Key) != ConsoleKey.Escape)
+        { 
+            Write("\tEnter one letter: ");
+            string input = ReadLine();
+            if (string.IsNullOrEmpty(input))
+            {
+                continue;
+            }
+            one_letter = input[0];
+            if (char.IsLower(one_letter))
+            {
+                WriteLine("\tResult: " + char.ToUpper(one_letter));
+            }
+            else if (char.IsUpper(one_letter))
+            {
+                WriteLine("\tResult: " + char.ToLower(one_letter));
+            }
+            WriteLine("\t\tEsc - Exit\t\tEnter - continue");
+
+        }
+
+    }
+}
